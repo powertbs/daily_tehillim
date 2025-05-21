b26c34ca6dd88deb86f8d3b1cf7ed306c11a5e8c
Mark_ab <54761289+powertbs@users.noreply.github.com>
2025-05-21 01:31:23 -0400

    Fixed some issues

--- custom_components/daily_tehillim/__init__.py
+++ custom_components/daily_tehillim/__init__.py
@@ -1,11 +1,19 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-
+from homeassistant.helpers.typing import ConfigType
 from .const import DOMAIN
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Daily Tehillim integration."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Daily Tehillim from a config entry."""
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")

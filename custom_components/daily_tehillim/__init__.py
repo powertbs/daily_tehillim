605229e7b0ef7340558a875ef919d891d6b76ff8
Mark_ab <54761289+powertbs@users.noreply.github.com>
2025-05-21 02:48:01 -0400

    fix

--- custom_components/daily_tehillim/__init__.py
+++ custom_components/daily_tehillim/__init__.py
@@ -3,24 +3,22 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers import entity_platform
 
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     return True
 
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    # Schedule the platform loading properly to avoid blocking the event loop
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
     return True
 
-
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    return await hass.config_entries.async_forward_entry_unloads(entry, ["sensor"])
+
 
